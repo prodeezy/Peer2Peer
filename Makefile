7372b94ae8a35b2bd35ed0137687ee3d10311f7e
gkowshik <gautamkowshik@gmail.com>
2012-03-25 21:44:23 -0700

    Makefile

--- Makefile
+++ Makefile
@@ -2,12 +2,12 @@
 
 CC = g++
 CFLAGS = -g -Wall -D_REENTRANT
-#OBJ = main.o keyboard.o timer.o iniParser.o incomingConnectionsHandle.o outgoingConnectionsHandler.o signalHandler.o keepAliveTimer.o logEntry.o connectBeacon.o indexSearch.o metaData.o
-OBJ = main.o incomingConnectionsHandle.o outgoingConnectionsHandler.o 
-LIBS = -lcrypto -lpthread
-INC = 
-#LIBS = -L/home.scf-22/csci551b/openssl/lib -lcrypto -lnsl -lsocket -lresolv
-#INC = -I/home/scf-22/csci551b/openssl/include
+#OBJ = main.o keyboard.o timer.o iniParser.o incomingConnectionsHandle.o outgoingConnectionsHandler.o signalHandler.o keepAlive.o logEntry.o beaconConnect.o indexSearch.o metaData.o
+OBJ = main.o keyboardReader.o beaconConnect.o timer.o signalHandler.o iniParser.o  keepAlive.o incomingConnectionsHandle.o outgoingConnectionsHandler.o 
+#LIBS = -lcrypto -lpthread
+#INC = 
+LIBS = -L/home/scf-22/csci551b/openssl/lib -lcrypto -lnsl -lsocket -lresolv
+INC = -I/home/scf-22/csci551b/openssl/include
 
 # Explicit rule
 all: sv_node
@@ -16,32 +16,32 @@ sv_node: $(OBJ)
 	$(CC) $(CFLAGS) -o sv_node $(OBJ) $(INC) $(LIBS) 
 
 install:
-
+	
 clean:
 	rm -rf *.o sv_node 
 
-main.o: main.cc
-	$(CC) $(CFLAGS) -c main.cc $(INC)
-#keyboard.o: keyboard.cc
-#	$(CC) $(CFLAGS) -c keyboard.cc $(INC)
-#timer.o: timer.cc
-#	$(CC) $(CFLAGS) -c timer.cc $(INC)
-#iniParser.o: iniParser.cc
-#	$(CC) $(CFLAGS) -c iniParser.cc $(INC)
-incomingConnectionsHandle.o: incomingConnectionsHandle.cc
-	$(CC) $(CFLAGS) -c incomingConnectionsHandle.cc $(INC)
-outgoingConnectionsHandler.o: outgoingConnectionsHandler.cc
-	$(CC) $(CFLAGS) -c outgoingConnectionsHandler.cc $(INC)
-#signalHandler.o: signalHandler.cc
-#	$(CC) $(CFLAGS) -c signalHandler.cc $(INC)
-#keepAliveTimer.o: keepAliveTimer.cc
-#	$(CC) $(CFLAGS) -c keepAliveTimer.cc $(INC)
-#logEntry.o: logEntry.cc
-#	$(CC) $(CFLAGS) -c logEntry.cc $(INC)
-#connectBeacon.o: connectBeacon.cc
-#	$(CC) $(CFLAGS) -c connectBeacon.cc $(INC)
-#indexSearch.o: indexSearch.cc
-#	$(CC) $(CFLAGS) -c indexSearch.cc $(INC)
-#metaData.o: metaData.cc
-#	$(CC) $(CFLAGS) -c metaData.cc $(INC)
+main.o: main.cpp
+	$(CC) $(CFLAGS) -c main.cpp $(INC)
+timer.o: timer.cpp
+	$(CC) $(CFLAGS) -c timer.cpp $(INC)
+iniParser.o: iniParser.cpp
+	$(CC) $(CFLAGS) -c iniParser.cpp $(INC)
+keyboardReader.o: keyboardReader.cpp
+	$(CC) $(CFLAGS) -c keyboardReader.cpp $(INC)	
+incomingConnectionsHandle.o: incomingConnectionsHandle.cpp
+	$(CC) $(CFLAGS) -c incomingConnectionsHandle.cpp $(INC)
+outgoingConnectionsHandler.o: outgoingConnectionsHandler.cpp
+	$(CC) $(CFLAGS) -c outgoingConnectionsHandler.cpp $(INC)
+signalHandler.o: signalHandler.cpp
+	$(CC) $(CFLAGS) -c signalHandler.cpp $(INC)
+keepAlive.o: keepAlive.cpp
+	$(CC) $(CFLAGS) -c keepAlive.cpp $(INC)
+#logEntry.o: logEntry.cpp
+#	$(CC) $(CFLAGS) -c logEntry.cpp $(INC)
+beaconConnect.o: beaconConnect.cpp
+	$(CC) $(CFLAGS) -c beaconConnect.cpp $(INC)
+#indexSearch.o: indexSearch.cpp
+#	$(CC) $(CFLAGS) -c indexSearch.cpp $(INC)
+#metaData.o: metaData.cpp
+#	$(CC) $(CFLAGS) -c metaData.cpp $(INC)
 
--- main.h
+++ main.h
@@ -8,7 +8,7 @@
 #include <sys/socket.h>
 #include <signal.h>
 #include <list>
-//#include <openssl/sha.h>
+#include <openssl/sha.h>
 #include <stdlib.h>
 #include <string.h>
 #include <netdb.h>
@@ -33,7 +33,6 @@ using namespace std ;
  * Message structure that goes into the messgae queues
  */
 
-
 struct MetaData
 {
         uint32_t location;
@@ -43,18 +42,24 @@ struct MetaData
         unsigned char homeDir[512];
         unsigned char logFileName[256];
         unsigned int autoShutDown;
-//        unsigned int autoShutDown_permanent;
+        unsigned int autoShutDownFixed;
+
         uint8_t ttl;
+        unsigned int joinTimeOutFixed;
+        unsigned int joinTimeOut;
+        unsigned int keepAliveTimeOut;
+
         unsigned int msgLifeTime;
         unsigned int getMsgLifeTime;
         unsigned int initNeighbor;
-        unsigned int joinTimeOut;
-        unsigned int joinTimeOut_permanent;
-        unsigned int keepAliveTimeOut;
+
         unsigned int minNeighbor;
         unsigned int noCheck;
         unsigned int cacheSize;
-        unsigned int retry;
+        double cacheProb;
+        double storeProb;
+        double neighborStoreProb;
+        unsigned int beaconRetryTime;
         list<struct NodeInfo *> *beaconList;
 
         bool isBeacon;                                          // to check if the current node is beacon or not
@@ -65,8 +70,6 @@ struct MetaData
         unsigned int statusResponseTimeout ;
         unsigned int checkResponseTimeout ;
 };
-
-
 
 struct NodeInfo {
 
@@ -89,10 +92,14 @@ struct NodeInfo {
 struct CachePacket {
 
 	struct NodeInfo sender ;
-        int status;                               
-        int reqSockfd ;
-        int msgLifeTime;
-        int status_type ;
+	/**
+	 *  0 - originated from here
+	 *  1 - The message was created by the node which
+	 **/
+	int status ;
+	int reqSockfd ;
+	int msgLifetimeInCache ;
+	int status_type ;
 };
 
 struct Message{
@@ -118,14 +125,12 @@ struct ConnectionInfo{
         int myWriteId;
         int isReady;
         bool joinFlag;
-        struct NodeInfo n;
+        struct NodeInfo neighbourNode;
 
         list<struct Message> MessageQ ;
         pthread_mutex_t mQLock ;
         pthread_cond_t mQCv ;
 };
-
-set<struct JoinResponseInfo> joinResponses ;
 
 
 struct JoinResponseInfo{
@@ -135,28 +140,96 @@ struct JoinResponseInfo{
         uint32_t location ;
 
         bool operator<(const struct JoinResponseInfo& aNode) const{
-                return aNode.location > location ;
+                return location < aNode.location ;
         }
 
         bool operator==(const struct JoinResponseInfo& aNode) const{
-                return ((aNode.neighbourPortNo == neighbourPortNo) && !strcmp((char *)aNode.neighbourHostname, (char *)neighbourHostname) && (aNode.location == location)) ;
+
+        	return ((aNode.neighbourPortNo == neighbourPortNo)
+        					&& !strcmp((char *)aNode.neighbourHostname, (char *)neighbourHostname)
+        					&& (aNode.location == location)) ;
+        	/**
+                return ((aNode.neighbourPortNo == neighbourPortNo)
+                			&& !strcmp((char *)aNode.neighbourHostname, (char *)neighbourHostname)
+//                			&& (aNode.location == location)) ; **/
         }
 };
 
 
-map<string, struct CachePacket> MessageCache ;
-pthread_mutex_t msgCacheLock ;
-
-map<struct NodeInfo, int> neighbourNodeMap ;
-pthread_mutex_t neighbourNodeMapLock ;
-
-map<int, struct ConnectionInfo> ConnectionMap ;
-pthread_mutex_t connectionMapLock ;
-
-list<pthread_t > childThreadList;
-MetaData *metadata;
 
 
-//Accept connections thread's socket
-int nSocket_accept = 0;
-bool shutDown = 0 ;
+
+
+
+
+//Flags
+extern bool shutDownFlag;
+extern bool joinNetworkPhaseFlag;
+
+extern int keepAlivePid;
+extern list<struct JoinResponseInfo> joinResponses ;
+extern unsigned char initNeighboursFilePath[512];
+extern unsigned char logFilePath[512];
+extern map<string, struct CachePacket> MessageCache ;
+extern pthread_mutex_t msgCacheLock ;
+extern pthread_mutex_t logEntryLock ;
+extern map<struct NodeInfo, int> neighbourNodeMap ;
+extern pthread_mutex_t neighbourNodeMapLock ;
+extern map<int, struct ConnectionInfo> ConnectionMap ;
+extern pthread_mutex_t connectionMapLock ;
+extern list<pthread_t > childThreadList;
+extern MetaData *metadata;
+extern int acceptServerSocket ;
+extern int nodeProcessId;
+extern int acceptProcessId;
+//extern FILE *loggerRef = NULL;
+
+
+// For timer thread
+extern int statusTimerFlag ;
+extern pthread_mutex_t statusMsgLock ;
+extern pthread_cond_t statusMsgCV;
+extern int checkTimerFlag;
+extern int softRestartEnable;
+extern int joinTimeOutFlag;
+
+
+// Function declaration
+extern int connectTo(char *context,unsigned char *hostName, unsigned int portNum );
+extern void performJoinNetworkPhase() ;
+extern void safePushMessageinQ(char *, int, struct Message ) ;
+extern void fillInfoToConnectionMap(   int newreqSockfd,
+								int shutDown ,
+								unsigned int keepAliveTimer ,
+								int keepAliveTimeOut ,
+								int isReady ,
+								bool joinFlag,
+								NodeInfo n) ;
+extern unsigned char *GetUOID(char *obj_type, unsigned char *uoid_buf, long unsigned int uoid_buf_sz);
+extern void closeConnection(int reqSockfd);
+extern set< set<struct NodeInfo> > statusResponse;
+
+// Thread implementation functions
+extern void *timerThread(void *);
+extern void *nodeServerListenerThread(void *);
+extern void *connectionReaderThread(void *args);
+extern void *connectionWriterThread(void *args);
+extern void *beaconConnectThread(void *args);
+extern void *keepAliveSenderThread(void *dummy);
+extern void *keyboardReader(void *dummy);
+
+
+extern void fillDefaultMetaData();
+extern void trim(unsigned char* line);
+extern void parseINI(char *fileName);
+extern void printMetaData();
+extern void setNodeInstanceId();
+extern void signalHandler(int sig);
+extern void waitForChildThreadsToFinish(char *context);
+extern void writeLogEntry(unsigned char *tobewrittendata);
+extern unsigned char *createLogRecord(unsigned char mode, int sockfd, unsigned char header[], unsigned char *buffer);
+extern void writeToStatusFile();
+extern int isBeaconNode(struct NodeInfo me);
+extern void getStatus();
+extern void notifyMessageSend(int resSock, uint8_t errorCode);
+extern void initiateNotify(int errorCode);
